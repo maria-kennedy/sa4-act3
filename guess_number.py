b3ec3cd51498d684ea2d80e7f8a2c99ba39406ed
Maria Kennedy <mariakingsbury1@gmail.com>
2024-02-10 14:54:52 -0600

    Added cap on number of guesses

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,10 @@
-number = '10'
+number = 10
 print("I'm thinking of a number...")
 guess = 0
+max_guesses = 5 
+remaining_guesses = max_guesses
 
-while guess != 'q':   
+while remaining_guesses > 0:   
     
     guess = input("What number am I thinking of? (or q to exit) ")
     
@@ -10,9 +12,18 @@ while guess != 'q':
         print(f"The number was {number}. \nBye!")
         break
 
-    elif guess == number:
-        print("Congratulations! You guessed the right number.")
+    elif guess.isdigit():
+        guess = int(guess)
+        remaining_guesses -= 1
+
+        if guess == number:
+            print('Congratulations! You guessed the right number.')
+            break
+        elif guess < number: 
+            print(f'Too low, {remaining_guesses} guesses left.')
+        else:
+            print(f'Too high, {remaining_guesses} guesses left.')
+
+    if remaining_guesses == 0:
+        print(f'Sorry, you have used all your guesses. The number was {number}.')
         break
-    
-    else:
-        print("Try again!")
